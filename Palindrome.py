4ab0ae9d939b1eb125c9e148ab1df6f8ff549e51
Kaleb Mathewos <94765666+McKaleb@users.noreply.github.com>
2022-12-29 16:39:25 +0300

    LeetCode

---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        xList = [*str(x)]
+        xList.reverse()
+        y = "".join(xList)
+        if str(x) == y:
+            return 1
+        else:
+            return 0
